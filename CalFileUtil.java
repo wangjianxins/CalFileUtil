aeb161f977dd33ee7be8df80bb27336a07c63b2f
王建新 <731461008@qq.com>
2017-06-28 14:07:04 +0800

    Update CalFileUtil.java

--- CalFileUtil.java
+++ CalFileUtil.java
@@ -1,1 +1,26 @@
-CalFileUtil
+public  class CalFileUtil {
+
+    private static void calFile(File file, int floor) throws Exception{
+
+        if(file.isDirectory()) {
+            for(File temp : file.listFiles()) {
+                //循环，如果是文件，文件层数加一
+                calFile(temp, floor + 1);
+            }
+        }
+        else {
+            for(int i = 1; i < floor; i++) {
+                System.out.print("\t");
+            }
+            System.out.println(file.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            calFile(new File("C:\\Users\\Administrator\\Desktop\\ing"),0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
